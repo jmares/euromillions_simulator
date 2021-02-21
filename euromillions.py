35ad34567c27a91985388c0b72d584211c26388b
jmares <67288966+jmares@users.noreply.github.com>
2021-02-21 13:41:56 +0100

    modified variable names and code - changed some variables to constants - chose some better names - added comments - modified status messages - replaced while loop with a for loop - removed duplicate initializations - ...

--- euromillions.py
+++ euromillions.py
@@ -9,32 +9,37 @@ print('******************************\n')
 print(strftime("%H:%M:%S", localtime()) + '\tStart')
 
 begin_time = datetime.datetime.now()
-numbers = list()
-stars = list()
 draws = dict()
-amount = 0
-simulations = 1_000_000
-display_results = 25
-display_status_draws = 100_000
+SIMULATIONS = 1_000_000        # number of simulations
+DISPLAY_RESULTS = 25           # display the top x results 
+DISPLAY_STATUS_DRAWS = 50_000  # display a status message every x draws
 
 print(strftime("%H:%M:%S", localtime()) + '\tDraws')
 
-while amount < simulations:
+#while amount < SIMULATIONS:
+for simulation in range(SIMULATIONS):
     numbers = []
-    stars = []
     while len(numbers) < 5:
         number = random.randint(1,50)
         if number not in numbers: numbers.append(number)
 
+    stars = []
     while len(stars) < 2:
         star = random.randint(1,12)
         if star not in stars: stars.append(star)
+
     numbers.sort()
     stars.sort()
-    amount += 1
-    draw = ''.join(str(numbers)) + ''.join(str(stars))
+    #amount += 1
+    #draw = ''.join(str(numbers)) + ''.join(str(stars))
+    #draw = str(numbers) + str(stars)
+    #draw = ', '.join(numbers) + ' | ' + ', '.join(stars)
+    draw = f"{numbers}{stars}"
+
     draws[draw] = draws.get(draw, 0) + 1
-    if amount % display_status_draws == 0 : print('{:10d}'.format(amount))
+    if simulation % DISPLAY_STATUS_DRAWS == 0 : print('{:10d}'.format(simulation))
+
+print('{:10d}'.format(SIMULATIONS))
 
 draws_list = list()
 
@@ -50,10 +55,10 @@ print(strftime("%H:%M:%S", localtime()) + '\tSorting ...\n')
 
 draws_list.sort(reverse=True)
 
-print(strftime("%H:%M:%S", localtime()) + f"\tDisplay top {display_results} results:\n")
-for draw in draws_list[:display_results]:
+print(strftime("%H:%M:%S", localtime()) + f"\tDisplay top {DISPLAY_RESULTS} results:\n")
+for draw in draws_list[:DISPLAY_RESULTS]:
     print(draw[0], ':', draw[1])
 
-print(strftime("%H:%M:%S", localtime()) + '\tEnd')
+print(strftime("\n" + "%H:%M:%S", localtime()) + '\tEnd')
 end_time = datetime.datetime.now()
-print("Execution time (h:m:s):", str(end_time - begin_time).split('.')[0])
+print("\nExecution time (h:m:s):", str(end_time - begin_time).split('.')[0])
